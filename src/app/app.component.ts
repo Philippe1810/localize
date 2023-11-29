a256847af0ec309c3ba9be91cbf8a51acc818c98
Philippe <philippe.carbonera@outlook.com>
2023-11-28 21:34:52 -0300

    Atualizações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SplashScreen } from '@awesome-cordova-plugins/splash-screen/ngx';
 import { StatusBar } from '@awesome-cordova-plugins/status-bar/ngx';
 import { Platform } from '@ionic/angular';
 import { DatabaseService } from './core/service/database.service';
 import { LoadingController } from '@ionic/angular';
+import { SQLiteDatabaseConfig } from '@awesome-cordova-plugins/sqlite/ngx';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+ selector: 'app-root',
+ templateUrl: 'app.component.html',
+ styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor(
+export class AppComponent implements OnInit {
+ constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
     private db: DatabaseService,
     private loadingController: LoadingController
-  ) {
+ ) {
     this.initializeApp();
-  }
+ }
 
-  async initializeApp() {
+ async ngOnInit() {
+    this.initializeDatabase();
+ }
+
+ async initializeApp() {
     this.platform.ready().then(async () => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
@@ -41,5 +46,15 @@ export class AppComponent {
         await loading.dismiss();
       }
     });
-  }
+ }
+
+ async initializeDatabase() {
+ // Abra o banco de dados.
+ try {
+    await this.db.openDatabase();
+    console.log('Banco de dados aberto com sucesso!');
+ } catch (error) {
+    console.error('Erro ao abrir o banco de dados:', error);
+ }
+}
 }
